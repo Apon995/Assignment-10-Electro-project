a11cac59f406dd49e6255d4720e15755a96d6e88
apon995 <shofikulsamz995@gmail.com>
2023-10-19 06:21:57 +0600

    navbar-and down menubar search bar make

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Logo from '../assets/Logo.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className='w-full h-[300px] bg-inherit'>
+
+        <div>
+          <div className='flex items-center '>
+            <img src={Logo} alt="" className='w-[70px] h-[60px] bg-inherit' />
+            <h1 className='text-2xl font-medium text-black'>electro</h1>
+          </div>
+        </div>
+
+
+
+
+      </div>
+
+
+
+
+
+    </>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <>
+        <div >
 
             <nav className='flex w-full items-center justify-between py-2 px-[5%] bg-[#fafafa]'>
                 <div className='flex items-center '>
@@ -33,7 +33,30 @@ function Navbar() {
                 </div>
             </nav>
 
-        </>
+            <div className='w-full bg-[#2742fd] px-[5%] flex items-center justify-between top-0 sticky'>
+
+                <div className='flex items-center gap-2 text-[#ffffff] py-6 '>
+                    <i className="fa-solid fa-square-caret-down"></i>
+                    <h1 className='font-normal text-base cursor-pointer '>All categories</h1>
+                </div>
+
+                <div className='flex border-[1px] h-[45px] w-[29rem] rounded-md'>
+                    <input type="text" name="search" id="search" placeholder='search products by name' className='w-[85%] px-2 rounded-l-sm' />
+                    <button className='w-[15%] bg-[#fafafa] rounded-r-sm text-xl hover:text-[#2742fd]'><i className="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+
+                <div className='flex items-center gap-2  text-[#ffffff] cursor-pointer'>
+                <i className="fa-solid fa-cart-shopping text-2xl"></i>
+                <h1 className='text-base font-medium'>Cart</h1>
+
+                    
+                    
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
